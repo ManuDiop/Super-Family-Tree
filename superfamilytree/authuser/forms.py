3b73d9c4e988a554e0ec4033dc51493af3a00881
ManuDiop <emmanuel.diop@gmail.com>
2024-08-07 14:27:09 +0200

    Fix de la page de login

--- superfamilytree/authuser/forms.py
+++ superfamilytree/authuser/forms.py
@@ -13,5 +13,5 @@ class CustomUserChangeForm(UserChangeForm):
         fields = ('email', 'first_name', 'last_name')
 
 class LoginForm(forms.Form):
-    email = forms.EmailField(label='Email')
+    email = forms.EmailField(widget=forms.EmailInput,label='Email')
     password = forms.CharField(widget=forms.PasswordInput, label='Password')
